6cbcecb1fef95dff7445fc5545e3253096afed0d
huong-allinfra <nguyen.le.thien.huong@allinfra.com>
2023-07-06 07:48:14 +0000

    minhem vpc

--- main.tf
+++ main.tf
@@ -15,11 +15,6 @@ terraform {
   }
 }
 
-variable "GLOBAL_AWS_ACCESS_KEY_ID" {}
-variable "GLOBAL_AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-
 provider "aws" {
   region = "ap-southeast-1"
 }
@@ -31,11 +26,28 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+provider "aws" {
+  alias = "minhem"
+  access_key = var.minh_AWS_ACCESS_KEY_ID
+  secret_key = var.minh_AWS_SECRET_ACCESS_KEY
+  region = "ap-southeast-2"
+}
+
 resource "aws_vpc" "test1" {
   provider = aws.global
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
     Name = "test1"
+  }
+}
+
+
+resource "aws_vpc" "minhem" {
+  provider = aws.minhem
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "minhem"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+
+variable "GLOBAL_AWS_ACCESS_KEY_ID" {}
+variable "GLOBAL_AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+variable "minh_AWS_ACCESS_KEY_ID" {}
+variable "minh_AWS_SECRET_ACCESS_KEY" {}
